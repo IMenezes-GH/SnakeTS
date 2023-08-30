d18721a299b7930435537133cbb0aa2f2baad5ef
IMenezes <107199192+IMenezes-GH@users.noreply.github.com>
2023-08-30 17:13:36 -0300

    Adds P keypress event for debugging purposes

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,39 @@
 const canvas: HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement
 const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!
 
+// CONFIG ==========================================
 const MS_PER_FRAME: number = 166
+
+// ==================================================
 
 async function sleep(timeMS : number): Promise<any>{
     return new Promise(resolve => setTimeout(resolve, timeMS))
 }
 
-async function gameLoop(){
-    let gameLoop : boolean = true
+/**
+ * Main Game loop
+ */
+async function gameLoop(): Promise<void>{
+    let loop : boolean = true
 
-    while(gameLoop){
+    while(loop){
 
         await sleep(MS_PER_FRAME)
+        console.log(loop)
+
+        // KEY EVENTS ========================================
+        document.addEventListener(('keypress'), (event) => {
+    
+            const KEY: string = event.key.toUpperCase()
+    
+            switch (KEY){
+                // DEBUG CASES ====================
+                case 'P':
+                    loop = false
+                    break
+            }
+    
+        })
     }
 }
 
