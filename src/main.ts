c98b9806075ae1d46a5a075a5d91824c8d1b2322
IMenezes <107199192+IMenezes-GH@users.noreply.github.com>
2023-08-30 16:59:48 -0300

    removes ctx.fillrect line from main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!
 
 const MS_PER_FRAME: number = 166
 
-ctx.fillRect(0, 0, canvas.width, canvas.height)
-
 async function sleep(timeMS : number): Promise<any>{
     return new Promise(resolve => setTimeout(resolve, timeMS))
 }
