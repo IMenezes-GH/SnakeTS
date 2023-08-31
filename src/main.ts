38c2487ddf11b7728fea36a8bb623cbb870190f9
IMenezes <107199192+IMenezes-GH@users.noreply.github.com>
2023-08-31 20:39:23 -0300

    Refactors gameLoop so the eventListener works outside of the game loop, allowing it to handle pause and continue properly

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,18 @@ import { BodySegment, Head, Snake } from "./snake.js"
 const canvas: HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement
 const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!
 
-// CONFIG ==========================================
+// CONFIGURATION ==========================================
 
 const MS_PER_FRAME: number = 30
+let loop : boolean = true
+
 canvas.height = window.innerHeight * 0.9 // SQUARE LAYOUT
 canvas.width = window.innerHeight * 0.9 // SQUARE LAYOUT
 
 async function sleep(timeMS : number): Promise<any>{
     return new Promise(resolve => setTimeout(resolve, timeMS))
 }
+
 // ==================================================
 // SNAKE CREATION ==============================================
 
@@ -21,12 +24,60 @@ for (let i = 0; i < 4; i++){
     Snake.body.push(new BodySegment({x: canvas.width / 2, y: canvas.height / 2}, ctx))
 }
 
+
+// KEY EVENTS ========================================
+document.addEventListener(('keydown'), (event) => {
+
+    const KEY: string = event.key.toUpperCase()
+
+    switch (KEY){
+        case 'D':
+        case 'ARROWRIGHT':
+            Snake.head.setMovement({x: 1, y: 0})
+
+            break
+
+        case 'W':
+        case 'ARROWUP':
+            Snake.head.setMovement({x: 0, y: -1})
+
+            break
+
+        case 'A':
+        case 'ARROWLEFT':
+            Snake.head.setMovement({x: -1, y: 0})
+
+            break
+
+        case 'S':
+        case 'ARROWDOWN':
+            Snake.head.setMovement({x: 0, y: 1})
+
+            break
+
+        // DEBUG CASES ========================================
+        case 'P':
+            loop = false
+            break
+
+        case 'R':
+            loop = true
+            gameLoop()
+            break
+
+        case 'E':
+            Snake.body.push(new BodySegment({x: Snake.head.x, y: Snake.head.y}, ctx))
+            break
+    }
+})
+
 /**
- * Main Game loop
+ * Game loop events
 */
 async function gameLoop(): Promise<void>{
-    let loop : boolean = true
-    while(loop){
+    
+    while (loop){
+
         await sleep(MS_PER_FRAME)
         ctx.clearRect(0, 0, canvas.width, canvas.height)
         
@@ -45,43 +96,6 @@ async function gameLoop(): Promise<void>{
         if (distanceOfWall.x <= 20 || distanceOfWall.y <= 20){
             loop = false
         } 
-        
-        // KEY EVENTS ========================================
-        document.addEventListener(('keydown'), (event) => {
-    
-            const KEY: string = event.key.toUpperCase()
-
-            switch (KEY){
-                case 'D':
-                case 'ARROWRIGHT':
-                    Snake.head.setMovement({x: 1, y: 0})
-
-                    break
-
-                case 'W':
-                case 'ARROWUP':
-                    Snake.head.setMovement({x: 0, y: -1})
-
-                    break
-
-                case 'A':
-                case 'ARROWLEFT':
-                    Snake.head.setMovement({x: -1, y: 0})
-
-                    break
-
-                case 'S':
-                case 'ARROWDOWN':
-                    Snake.head.setMovement({x: 0, y: 1})
-
-                    break
-
-                // DEBUG CASES ====================
-                case 'P':
-                    loop = false
-                    break
-            }
-        })
     }
 }
 
