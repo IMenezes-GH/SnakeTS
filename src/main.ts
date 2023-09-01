ebfe6c2b4a3c20e3bf8de92cb058dfa1d84080c7
IMenezes <107199192+IMenezes-GH@users.noreply.github.com>
2023-09-01 15:06:11 -0300

    Changes pellet spawning are multiplies to 0.8

--- src/main.ts
+++ src/main.ts
@@ -121,8 +121,8 @@ async function gameLoop(): Promise<void> {
                 Snake.addBody(new BodySegment(Food.list[0].coordinates))
                 
                 Food.list[0] = new Pellet({
-                    x: Math.floor(canvas.width * 0.1 + Math.random() * canvas.width * 0.9),
-                     y: Math.floor(canvas.width * 0.1 + Math.random() * canvas.height * 0.9)})
+                    x: Math.floor(canvas.width * 0.1 + Math.random() * canvas.width * 0.8),
+                     y: Math.floor(canvas.width * 0.1 + Math.random() * canvas.height * 0.8)})
             }
             
     }
